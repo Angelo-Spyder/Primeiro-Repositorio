772dec7def3e4a9177eb38f422efeb6448948397
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-03-20 22:22:48 -0300

    Adiconando elementos na tabela

--- main.js
+++ main.js
@@ -5,5 +5,16 @@ var fruta = document.querySelector("#frutaInput");
 var botaoAdiciona = document.querySelector("#botaoAdiciona");
 
 botaoAdiciona.addEventListener("click", function(){
-    
-})
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+
+    tr.classList.add("fruta-linha")
+    td.classList.add("fruta-iten");
+
+    td.textContent = fruta.value;
+    tr.appendChild(td);
+
+    tabela.appendChild(tr)
+
+    fruta.value = ""
+});

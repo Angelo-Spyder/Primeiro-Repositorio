e3227057cc613d1e8a554708dddd2cb5218906a8
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-03-21 13:52:17 -0300

    Separando funções da construção da tabela

--- main.js
+++ main.js
@@ -5,16 +5,26 @@ var fruta = document.querySelector("#frutaInput");
 var botaoAdiciona = document.querySelector("#botaoAdiciona");
 
 botaoAdiciona.addEventListener("click", function(){
-    var tr = document.createElement("tr");
-    var td = document.createElement("td");
+    var tr = criaTr();
+    var td = criaTd();
 
-    tr.classList.add("fruta-linha")
-    td.classList.add("fruta-iten");
-
-    td.textContent = fruta.value;
     tr.appendChild(td);
-
     tabela.appendChild(tr)
 
     fruta.value = ""
 });
+
+function criaTr(){
+    var tr = document.createElement("tr");
+    tr.classList.add("fruta-linha");
+
+    return tr;
+}
+
+function criaTd(){
+    var td = document.createElement("td");
+    td.classList.add("fruta-iten");
+    td.textContent = fruta.value;
+
+    return td;
+}

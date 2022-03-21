3c4d52e784a3f3e75304f81f8853d5400db1ca23
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-03-20 22:19:13 -0300

    Captuando dados do input

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var tabela = document.querySelector(".tabela");
+
+var fruta = document.querySelector("#frutaInput");
+var botaoAdiciona = document.querySelector("#botaoAdiciona");
+
+botaoAdiciona.addEventListener("click", function(){
+    
+})
